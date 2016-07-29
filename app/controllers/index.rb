627e417f8f4c4396fb4d65a5873a805e0f0b8e02
alfredlam42 <alfred.lam.42@gmail.com>
2016-07-28 22:12:04 -0700

    version 1

---
+++ app/controllers/index.rb
@@ -0,0 +1,16 @@
+get '/' do
+  erb :'/index'
+end
+
+get '/search' do
+  item = params[:item].split(' ').join('+')
+
+  if params[:completed]
+    path = 'http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findCompletedItems&SERVICE-VERSION=1.7.0&SECURITY-APPNAME=AlfredLa-AverageP-PRD-513dad11d-6b1d4d75&RESPONSE-DATA-FORMAT=XML&REST-PAYLOAD&keywords=' + item + '&itemFilter(0).name=SoldItemsOnly&itemFilter(0).value=true&sortOrder=StartTimeNewest&paginationInput.entriesPerPage=50'
+  else
+    path = 'http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.7.0&SECURITY-APPNAME=AlfredLa-AverageP-PRD-513dad11d-6b1d4d75&RESPONSE-DATA-FORMAT=XML&REST-PAYLOAD&keywords=' + item + '&itemFilter(0).name=ListingType&itemFilter(0).value=FixedPrice&sortOrder=PricePlusShippingLowest&paginationInput.entriesPerPage=50'
+  end
+
+  response = HTTParty.get(path)
+  response.to_json
+end
