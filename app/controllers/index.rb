e72e0976caba5ec71b1336628ab5de8a9d0d5789
alfredlam42 <alfred.lam.42@gmail.com>
2016-07-28 22:49:26 -0700

    add controller

---
+++ app/controllers/index.rb
@@ -0,0 +1,17 @@
+get '/' do
+  erb :'/index'
+end
+
+get '/search' do
+  item = params[:item].split(' ').join('+')
+
+  if params[:completed]
+    path = 'http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findCompletedItems&SERVICE-VERSION=1.7.0&SECURITY-APPNAME=' + ENV['DEV_ID'] + '&RESPONSE-DATA-FORMAT=XML&REST-PAYLOAD&keywords=' + item + '&itemFilter(0).name=SoldItemsOnly&itemFilter(0).value=true&sortOrder=StartTimeNewest&paginationInput.entriesPerPage=50'
+  else
+    path = 'http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.7.0&SECURITY-APPNAME=' + ENV['DEV_ID'] + '&RESPONSE-DATA-FORMAT=XML&REST-PAYLOAD&keywords=' + item + '&itemFilter(0).name=ListingType&itemFilter(0).value=FixedPrice&sortOrder=PricePlusShippingLowest&paginationInput.entriesPerPage=50'
+  end
+
+  response = HTTParty.get(path)
+  response.to_json
+end
+Contact GitHub API Training Shop Blog About
