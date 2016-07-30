a64b4a43109a4aee28dbbb07c51073f3f0db53d0
alfredlam42 <alfred.lam.42@gmail.com>
2016-07-30 00:12:10 -0700

    remove dotenv for heroku

--- config/environment.rb
+++ config/environment.rb
@@ -22,8 +22,6 @@ require "sinatra/reloader" if development?
 
 require 'erb'
 require 'httparty'
-require 'dotenv'
-Dotenv.load
 
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
